984905b8b8335e7812e3d94a607bf4de6f0d4320
Joshua Rosales <joshuarosalesmoreno@hotmail.com>
2025-11-14 04:16:30 -0500

    update

--- configuration.nix
+++ configuration.nix
@@ -14,10 +14,7 @@
 
   time.timeZone = "America/Lima";
   i18n.defaultLocale = "es_PE.UTF-8";
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+
   users.users.loonbac = {
     isNormalUser = true;
     description = "Joshua Rosales";
@@ -26,6 +23,25 @@
   };
   nixpkgs.config.allowUnfree = true;
 
+#WAYLAND CON LABWC
+  services.xserver.enable = false;
+  programs.labwc.enable = true;
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+##################
 
   environment.systemPackages = with pkgs; [
     vim
@@ -35,6 +51,18 @@
     tealdeer
     xclip
     bat
+    alacritty
+    fastfetch
+    vscode
+    firefox
+
+    labwc
+    waybar
+    foot
+    wlr-randr
+    kanshi
+    grim
+    slurp
   ];
 
   system.stateVersion = "25.05"; # Did you read the comment?
