01325da53a4a5c1d96f4f48fd1ce6cbbdf82cd51
Joshua Rosales <joshuarosalesmoreno@hotmail.com>
2025-11-14 05:35:41 -0500

    configuration nix

---
+++ home.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ...}:
+
+{
+    home.username = "loonbac";
+    home.homeDirectory = "/home/loonbac";
+    home.stateVersion = "25.05";
+    
+    # Permitir que home-manager gestione sí mismo
+    programs.home-manager.enable = true;
+    
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            btw = "echo uso wayland btw";
+            ll = "ls -lah";
+            gs = "git status";
+            rebuild = "sudo nixos-rebuild switch --impure --flake .";
+        };
+        # Mejoras para el prompt
+        initExtra = ''
+            # Prompt personalizado
+            PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+        '';
+    };
+}
